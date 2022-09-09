f5ea9de6e6104f55f80a2889eaa1ef5d91963345
PHELIPE QUEIROZ DE ARAUJO <u20368@LAPA23.academico.cotuca.unicamp.br>
2022-09-09 20:45:37 -0300

    primeiro commit

---
+++ Projeto_Escola/escola/src/components/template/Menu.js
@@ -0,0 +1,16 @@
+import './Menu.css';
+export default function Menu(props) {
+    return (
+    <nav className='menu'>
+    <a href="/Alunos">
+    Alunos
+    </a>
+    <a href="/Cursos">
+    Cursos
+    </a>
+    <a href="/Carometro">
+    Carômetro
+    </a>
+    </nav>
+    )
+    }
